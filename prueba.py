284e117086e6458f0d152804663638ecfb5cb0a8
Kirurai <kd.maurii@gmail.com>
2022-06-02 16:02:38 -0300

    Principio de las pruebas

---
+++ prueba.py
@@ -0,0 +1,21 @@
+numero = 111_111.52
+
+a = 2
+b = 3
+c = 0
+d = 5
+
+cantidad_numeros = 4
+
+promedio = ( a + b + c + d )/cantidad_numeros
+
+"""
+lo que quieran
+es lo que escribi
+"""
+int("3")
+float("3")
+str(4)
+print( "El promedio de " + str(promedio) )
+print( "Promedio de " + promedio )
+#print(promedio)
