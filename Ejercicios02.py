566a9fd2cd56c3006a0d7ab81581f9f68d489ae0
florenciaquinonez <florencia.quinonez@uala.com.ar>
2022-06-08 14:55:28 -0300

    Agrego titulos

--- Ejercicios02.py
+++ Ejercicios02.py
@@ -8,18 +8,23 @@ def promedio(lista):
 
 lista_a = [1, 2, 3, 4]
 
+print("Ejercicios Suma y Promedio\n")
 
 print(suma(1, 2))
-print(promedio(lista_a))
+print(promedio(lista_a), "\n")
 
 
 # 2. Iterar a través de un array y que muestre el promedio de los datos(minimo 10 elementos)
+
+print("Ejercicio imprimir lista\n")
 
 for elemento in lista_a:
     print(elemento)
 
 
 # 3. Iterar a traves de un diccionario con un for para imprimir un texto con ambos valores (Minimo 5 elementos)
+
+print("\nEjercicio imprimir diccionario\n")
 
 diccionario_A = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e"}
 for key, elemento in diccionario_A.items():
@@ -28,6 +33,8 @@ for key, elemento in diccionario_A.items():
 
 # 4. Realizar un while que agregue un caracter o un element a alguna variable, hasta que supere un largo de 8 
 # elementos (empezar de algo vacio). Imprimir cada iteracion.
+
+print("\n Ejercicio cadena de caracteres\n")
 
 variable = ""
 while len(variable) <= 8:
