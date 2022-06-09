6119d993128c54b9a9eb8ddc6119ee24926a1927
MaurizioMino <104390892+MaurizioMino@users.noreply.github.com>
2022-06-09 15:49:55 -0300

    Agregada cosas para la práctica

---
+++ clases.py
@@ -0,0 +1,31 @@
+from libr import suma
+
+class Persona:
+
+    personas = []
+
+    def __init__(self, nombre, documento):
+        self.name = nombre
+        self.dni = documento
+        Persona.personas.append(self)
+
+    def dar_info( self ):
+        return f'Name: {self.name}\nDni: {self.dni}\n'
+
+    @staticmethod
+    def contar_personas( ):
+        return len(Persona.personas)
+
+
+
+a = Persona("Pepito", 314159265)
+print(a.contar_personas())
+
+b = Persona("Jorge", 314159265)
+c = Persona("Pepito", 314159265)
+d = Persona("Pepito", 314159265)
+
+#print(a.contar_personas())
+#print(Persona.contar_personas())
+
+print(suma(2, 3))
